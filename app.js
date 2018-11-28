33fd1eb0f6efe5c5c9304086a51d759fc1b2f103
Sebastian <wwlochaty@gmail.com>
2018-11-28 17:33:35 +0100

    Setup environment variables and modify models to fix bugs

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const passport = require('passport')
+const session = require('express-session')
+const models = require('./models')
+
+const port = process.env.PORT || require('./config/config')[process.env.NODE_ENV].port 
+
 const app = express()
-const port = process.env.NODE_ENV || 3000;
 
 app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 
+app.use(session({
+  secret: 'its a secret',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/user', require('./routes/user'))
 
 app.get('/', (req, res) => {
-  res.render('pages/index', {title: 'Home'})
+  res.render('pages/index', {title: 'Home', user: req.user})
 })
-app.get('/login', (req, res) => {
-  res.render('pages/login', {title: 'Login'})
-})
-app.get('/signup', (req, res) => {
-  res.render('pages/signup', {title: 'Sign Up'})
-})
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+
+models.sequelize.sync({force: true}).then(() => {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+  })
+}).catch(err => {
+  console.log('Unable to sync db', err)
 })
