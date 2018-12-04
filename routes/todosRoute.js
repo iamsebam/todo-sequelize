391a271bcac14ef9e654c0bd1f9929478f813723
Sebastian <wwlochaty@gmail.com>
2018-12-04 16:36:29 +0100

    Create todos route

---
+++ routes/todosRoute.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+  , todosCtrl = require('../controllers/todosCtrl')
+  , userCtrl = require('../controllers/userCtrl')
+
+router.use(userCtrl.isLoggedIn)
+
+router.get('/', todosCtrl.todosView)
+router.post('/add', todosCtrl.addTodo)
+router.delete('/delete:id', todosCtrl.deleteTodo)
+router.patch('/update:id', todosCtrl.updateTodo)
+
+module.exports = router
