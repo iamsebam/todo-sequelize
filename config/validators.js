4b8dd6bcf2a49cfd9196fa4ebc8949994cc01413
Sebastian <wwlochaty@gmail.com>
2018-12-04 03:02:09 +0100

    Create validation chain

---
+++ config/validators.js
@@ -0,0 +1,31 @@
+const { body } = require('express-validator/check')
+  , User = require('../models').models.User
+
+module.exports = {
+  signUpData: [
+    body('username')
+      .not().isEmpty().withMessage('Username field is required.')
+      .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long.')
+      .isAlphanumeric().withMessage('Username must contain of only letters and numbers.')
+      .custom(async username => {
+        try {
+          const user = await User.findOne({ where: {username} })
+          if (user) {
+            return Promise.reject('Username already in use.')
+          }
+        } catch (err) {
+          return Promise.reject(err)
+        }
+      }),
+    body('password')
+      .not().isEmpty().withMessage('Password field is required.')
+      .isLength({ min: 4 }).withMessage('Password must be at least 4 characters long.')
+      .custom((password, { req }) => {
+        if (password !== req.body.password2) {
+          return false
+        } else {
+          return password
+        }
+      }).withMessage('Passwords do not match.')
+  ]
+}
