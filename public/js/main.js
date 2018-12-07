f06433821ba433facca9e679a304348d0452e1d4
Sebastian <wwlochaty@gmail.com>
2018-12-07 10:33:00 +0100

    Add event listeners to the navbar and todos page

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,115 @@
-document.querySelectorAll('.redirect').forEach(btn => {
-  btn.addEventListener('click', () => {
-    window.location.href = `/user/${btn.name}`
+document.addEventListener("DOMContentLoaded", () => {
+  const redirect = document.querySelectorAll('.redirect')
+  const home = document.querySelector('.home')
+  const logout = document.querySelector('.logout')
+  const todos = document.querySelector('#todos')
+  const todo = document.querySelectorAll('li')
+
+  if(redirect) {
+    redirect.forEach(btn => {
+      btn.addEventListener('click', () => {
+        window.location.href = `/user/${btn.name}`
+      })
+    })
+  }
+  if (home) {
+    home.addEventListener('click', () => {
+      window.location.href = '/'
+    })
+  }
+  if (logout) {
+    logout.addEventListener('click', () => {
+      window.location.href = '/user/logout'
+    })
+  }
+  if (todos) {
+    todos.addEventListener('click', () => {
+      window.location.href = '/todos'
+    })
+  }
+  if (todo) {
+    let id
+    todo.forEach(todo => {
+      todo.addEventListener('click', (e) => {  
+        if (e.target.matches('li')) { // Todo list item
+          let activeListItem = document.querySelector('.li-active')
+          if (!(e.target === activeListItem)) {
+            if (activeListItem) {
+              toggleListItemView(activeListItem) 
+            }
+          } 
+          toggleListItemView(e.target)
+        } else if (e.target.matches('.fa-trash-alt')) { // Delete button
+          id = e.target.parentElement.parentElement.dataset.todo_id.toString()
+          const req = new XMLHttpRequest()
+          req.open('DELETE', '/todos/delete/' + id, true)
+          req.onload = () => {
+            if(req.status >= 200 && req.status < 400) {
+              window.location.reload()
+            } else {
+              console.log(err)
+            }
+          }
+          req.onerror = () => {
+            console.log(err)
+          }
+          req.send()
+        } else if (e.target.matches('.fa-edit')) { // Edit button
+          let li = e.target.parentElement.parentElement    
+          let input = `<input type="text" class="editTodo-input" name="todo" placeholder="${li.textContent}" autocomplete="off" spellcheck="false"/><span class="todo-panel"><i class="far fa-save"></i><i class="fas fa-times"></i></span>`
+          li.textContent = ''
+          li.insertAdjacentHTML('afterbegin', input)
+        } else if (e.target.matches('.fa-check')) { // Check button
+          let li = e.target.parentElement.parentElement 
+          if (!li.classList.contains('li-active') && e.target.classList.contains('success')) {
+            e.target.classList.add('invisible')
+          }
+          e.target.classList.toggle('success')
+        } else if (e.target.matches('.fa-save')) { // Save button
+          id = e.target.parentElement.parentElement.dataset.todo_id.toString()
+          let input = document.querySelector('.editTodo-input')
+          const req = new XMLHttpRequest()
+          req.open('PATCH', '/todos/update/' + id, true)
+          req.setRequestHeader('Content-type','application/json; charset=utf-8')
+          req.onload = () => {
+            if(req.status >= 200 && req.status < 400) {
+              window.location.reload()
+            } else {
+              console.log(err)
+            }
+          }
+          req.onerror = () => {
+            console.log(err)
+          }
+          req.send(JSON.stringify({todo: input.value}))      
+        } else if (e.target.matches('.fa-times')) { // Cancel button
+          let todoContent = e.target.parentElement.parentElement.firstChild.getAttribute('placeholder')
+          let li = e.target.parentElement.parentElement
+          let panel = '<span class="todo-panel"><i class="fas fa-check"></i><i class="fas fa-edit"></i><i class="fas fa-trash-alt"></i></span>'
+          li.removeChild(li.firstChild)
+          li.textContent = todoContent
+          li.insertAdjacentHTML('beforeend', panel)
+        } 
+      })
+    })
+  }
+  window.addEventListener('click', e => {
+    let activeListItem = document.querySelector('.li-active')
+      if(!e.target.matches('.li-active')) {
+        if(activeListItem) {
+          toggleListItemView(activeListItem)
+        }
+      }
   })
 })
+
+function toggleListItemView (e) {
+  if (!e.firstChild.classList) {
+    e.classList.toggle('li-active')
+    e.lastChild.childNodes.forEach(el => {
+      if (!el.classList.contains('success')) {
+        el.classList.toggle('invisible')
+      }
+    })
+  }
+}
