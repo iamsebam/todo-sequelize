0dbc5c54c7bcf4bae8a862ece992c2e9919c2924
Sebastian <wwlochaty@gmail.com>
2019-01-02 21:16:30 +0100

    Fix index.js loading config module bug

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
-const config = require('../config/config')
+const dbURL = process.env.DATABASE_URL || require('../config/config')[process.env.NODE_ENV].url
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || config[process.env.NODE_ENV].url, {
+const sequelize = new Sequelize(dbURL, {
   dialect: 'postgres',
   timestamps: true,
   oparatorsAliases: false,
